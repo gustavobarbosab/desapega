bd9d056b60a9cbc7c99f4d18979da8373acdb940
Gustavo Barbosa Barreto <tqi_gbarreto@uolinc.com>
2022-07-27 19:07:32 -0300

    ajustando javascript

--- script.js
+++ script.js
@@ -1,52 +1,28 @@
-// window.onload = function () {
-//     const path = window.location.pathname.split("/");
+window.onload = function () {
+    document.querySelectorAll(".menu__item").forEach((item) => {
+        item.addEventListener("click", function () {
+            const path = item.getAttribute("value");
+            loadPage(path);
+        });
+    });
 
-//     switch (path[1]) {
-//         case "home":
-//             {
-//                 loadPage("home");
-//                 break;
-//             }
-//         case "login":
-//             {
-//                 loadPage("login");
-//                 break;
-//             }
-//         case "register":
-//             {
-//                 loadPage("register");
-//                 break;
-//             }
-//         default:
-//             {
-//                 loadPage("404");
-//                 break;
-//             }
-//     }
+    function loadPage(path) {
+        if (path == "") return;
 
-//     document.querySelectorAll(".menu__item").forEach((item) => {
-//         item.addEventListener("click", function () {
-//             const path = item.getAttribute("value");
-//             loadPage(path);
-//         });
-//     });
+        const container = document.getElementById("container");
 
-//     function loadPage($path) {
-//         if ($path == "") return;
-
-//         const container = document.getElementById("container");
-
-//         const request = new XMLHttpRequest();
-//         request.open("GET", "pages/" + $path + "/index.html");
-//         request.onload = function () {
-//             if (request.status == 200) {
-//                 container.innerHTML = request.responseText;
-//                 document.title = $path;
-//             }
-//         };
-//         request.onerror = function(error) {
-//             console.log("Errooo " + error);
-//         };
-//         request.send();
-//     }
-// }
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "pages/" + path + "/index.html");
+        xhr.responseType = "document";
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                container.innerHTML = xhr.responseText;
+                // document.title = $path;
+            }
+        };
+        xhr.onerror = function(error) {
+            console.log("Errooo " + error);
+        };
+        xhr.send();
+    }
+}
