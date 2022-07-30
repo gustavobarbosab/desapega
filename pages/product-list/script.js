d29c4a23494851d5f039a521e772db164cccb97f
Harrisu <harrisufelipe@gmail.com>
2022-07-30 12:02:43 -0300

    Pagina deleção, att pagina listagem, adicionando menu lateral

--- pages/product-list/script.js
+++ pages/product-list/script.js
@@ -27,33 +27,60 @@ var produtos = [
         "name":"gabinete",
         "preco":"2541,00",
     },
+    {
+        "name":"notebook Slim",
+        "preco":"4000,00",
+    },
+    {
+        "name":"mouse",
+        "preco":"50,00",
+    },
+    {
+        "name":"teclado",
+        "preco":"500,00",
+    },
+    {
+        "name":"cadeira gamer super insana e colorida para toda a familia",
+        "preco":"7000,00",
+    },
+    {
+        "name":"mouse pad",
+        "preco":"40,00",
+    },
+    {
+        "name":"caderno",
+        "preco":"407,00",
+    },
+    {
+        "name":"gabinete",
+        "preco":"2541,00",
+    },
 ]
 
-window.onload = function(){
-
+const appendProducts = function () {
     let content = document.querySelector(".content");
+    let template = document.querySelector("#templateProduct");
 
     for(item of produtos){
 
-        content.innerHTML += `
-        <a href="" class="card-desapega">
-            <div class="card-product">
-                <img 
-                    src="../../commons/images/placeholder_product.jpg" 
-                    alt="imagemProduto" 
-                    width="200"
-                    height="160"
-                >
-                <div class="card-text">
-                    <h1>${item.name}</h1>
-                    <h2 id="price">R$ ${item.preco}</h2>
-                </div>
-            </div>
-            <p class="card-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis cum sit quam fugit, nulla animi laudantium totam ratione optio a tempora commodi obcaecati quibusdam labore assumenda dolor harum reiciendis eaque?
-            </p>
-        </a>
-        `
+        let Card = template.innerHTML
+          .replace("{{item.name}}", item.name)
+          .replace("{{item.preco}}", item.preco);
+
+        content.insertAdjacentHTML("beforeend", Card);
+
     }
+}
+
+
+window.onload = function(){
+
+    appendProducts();
 
 }
+
+window.onscroll = function () {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        
+    }
+  };
