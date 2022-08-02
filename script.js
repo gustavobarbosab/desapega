b467a3b7443c3c0593d1ef27c300b69d3926d0d0
Gustavo Barbosa Barreto <tqi_gbarreto@uolinc.com>
2022-08-02 20:41:49 -0300

    Criando layout tela de login

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    loadPage("home") // TODO remover
     document.querySelectorAll(".menu__item").forEach((item) => {
         item.addEventListener("click", function () {
             const path = item.getAttribute("value");
@@ -9,15 +10,14 @@ window.onload = function () {
     function loadPage(path) {
         if (path == "") return;
 
-        const container = document.getElementById("container");
+        const container = document.getElementById("main__container");
 
         const xhr = new XMLHttpRequest();
         xhr.open("GET", "pages/" + path + "/index.html");
-        // xhr.responseType = "document";
         xhr.onload = function () {
             if (xhr.status == 200) {
                 container.innerHTML = xhr.responseText;
-                // document.title = $path;
+                document.title = path;
             }
         };
         xhr.onerror = function(error) {
