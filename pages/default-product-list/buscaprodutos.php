7b5a39ca25d3328b1eb9c046be9280f8da7a747d
Gustavo Barbosa Barreto <tqi_gbarreto@uolinc.com>
2022-08-07 18:15:19 -0300

    criando pesquisa

--- pages/default-product-list/buscaprodutos.php
+++ pages/default-product-list/buscaprodutos.php
@@ -5,23 +5,23 @@ require "../../commons/php/autenticacao.php";
 
 session_start();
 $pdo = mysqlConnect();
-$pagina = $_GET['offset'];
+$offset = $_GET['offset'];
+$title = $_GET['title'] ?? "";
 
 header("Content-Type: application/json");
 try {
     $sql = <<<SQL
         SELECT * FROM anuncio
+        WHERE titulo LIKE :title
         ORDER BY data_hora
-        LIMIT 20 OFFSET ?;
+        LIMIT 20 OFFSET :offset;
     SQL;
 
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([$pagina]);
+    $stmt->execute([":title" => "%".$title."%",":offset" => $offset]);
     $dados = $stmt->fetchAll();
     echo json_encode($dados);
-
-}catch (Exception $ex) {
+} catch (Exception $ex) {
     http_response_code(500);
     echo json_encode(new RequestResponse(false, $ex->getMessage()));
 }
-?>
